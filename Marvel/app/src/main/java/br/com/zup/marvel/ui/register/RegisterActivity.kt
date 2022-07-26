180a74617ce9903b6eed31c63b5e21679d704ccc
jennysival <jeniffersival@gmail.com>
2022-07-26 11:07:56 -0300

    Criar activity de registro

---
+++ Marvel/app/src/main/java/br/com/zup/marvel/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package br.com.zup.marvel.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.marvel.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
